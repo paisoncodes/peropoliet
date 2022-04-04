2c598eceba0525a1cb8a71d88dfa9a8ec93c6f26
Preshh0 <uniquep201@gmail.com>
2022-04-04 13:47:27 +0100

    Made a few edits that i missed initially. Added the update button for the cart. Still haven't fixed the image issue + 'The Cart is empty' issue, but we've made progress.

--- store/views.py
+++ store/views.py
@@ -17,8 +17,7 @@ def product_list(request, category_slug=None):
                     'products':products})
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(Product,
-                                id = id,
+    product = get_object_or_404(Product, id = id,
                                 slug=slug,
                                 available=True)
     cart_product_form = CartAddProductForm()
