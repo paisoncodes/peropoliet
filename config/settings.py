25a6dd1fe53289fcba7af876f8e582cb7567d3b6
Preshh0 <uniquep201@gmail.com>
2022-04-09 23:47:26 +0100

    Made edits to the settings.py file so i'd see if rendering static files would work.

--- config/settings.py
+++ config/settings.py
@@ -127,6 +127,12 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'peropoliet/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+
 CART_SESSION_ID = 'cart'
