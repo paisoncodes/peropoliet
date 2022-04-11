2af54adcc64db3f6b0bb13345372fefd1fca1221
Preshh0 <42142405+Preshh0@users.noreply.github.com>
2022-04-11 09:25:08 +0100

    Had to fix the static root error on heroku.
    It keeps showing static root error, let's hope this change works.

--- myshop/settings.py
+++ myshop/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
